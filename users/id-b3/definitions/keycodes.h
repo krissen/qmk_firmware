99fdc00873abd5b70cd54a08af915d2f9186cb29
id-b3 <58172188+id-b3@users.noreply.github.com>
2022-08-28 21:57:42 +0200

    Updated base layout for home row mods.

---
+++ users/id-b3/definitions/keycodes.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "layers.h"
+
+// Home Row Mods
+#define HR_A LALT_T(KC_A)
+#define HR_R LCTL_T(KC_R)
+#define HR_S LGUI_T(KC_S)
+#define HR_T LSFT_T(KC_T)
+
+#define HR_O LALT_T(KC_O)
+#define HR_I LCTL_T(KC_I)
+#define HR_E LGUI_T(KC_E)
+#define HR_N LSFT_T(KC_N)
+
+// Shortcuts
+#define CADEL LALT(LCTL(KC_DELETE))
+#define TRML LALT(LCTL(KC_T))
+#define TSKMN LCTL(LSFT(KC_ESCAPE))
+#define CLSTAB LCTL(LSFT(KC_T))
+#define SNAP LGUI(LSFT(KC_S))
+#define SNAPL LGUI(LSFT(KC_LEFT))
+#define SNAPR LGUI(LSFT(KC_RGHT))
+#define FPASTE LALT(LCTL(KC_V))
+
--- users/id-b3/definitions/keymaps.h
+++ users/id-b3/definitions/keymaps.h
@@ -5,11 +5,11 @@
 #include "layers.h"
 
 #define ________COLEMKL1________    KC_Q, KC_W, KC_F, KC_P, KC_G
-#define ________COLEMKL2________    KC_A, KC_R, KC_S, KC_T, KC_D
+#define ________COLEMKL2________    HR_A, HR_R, HR_S, HR_T, KC_D
 #define ________COLEMKL3________    KC_Z, KC_X, KC_C, KC_V, KC_B
 
 #define ________COLEMKR1________    KC_J, KC_L, KC_U, KC_Y, KC_SCLN
-#define ________COLEMKR2________    KC_H, KC_N, KC_E, KC_I, KC_O
+#define ________COLEMKR2________    KC_H, HR_N, HR_E, HR_I, HR_O
 #define ________COLEMKR3________    KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH
 
 #define ____THL1____    MT(MOD_LSFT, KC_SPC), LT(_SYMNUM, KC_ENT), KC_ESC
@@ -23,4 +23,12 @@
 #define ________SYMNUMR2________    KC_NO, KC_4, KC_5, KC_6, KC_0 
 #define ________SYMNUMR3________    KC_NO, KC_1, KC_2, KC_3, KC_NO 
 
-#define ____THBK____    KC_NO, KC_NO, KC_NO
+#define ____THBK____    _______, _______, _______
+
+#define ________SHRTCTL1________    CADEL, C(KC_W), C(KC_F), SNAPL, SNAPR
+#define ________SHRTCTL2________    TRML,   _______,   SNAP,CLSTAB, _______
+#define ________SHRTCTL3________    TSKMN, _______, _______, FPASTE, _______
+
+#define ________SHRTCTR1________    KC_MYCM, _______, _______, _______, _______
+#define ________SHRTCTR2________    _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+#define ________SHRTCTR3________    _______, _______, _______, _______, G(KC_DOT)
