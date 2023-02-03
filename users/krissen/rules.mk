b28f1459e55f420bdf3d0a9f79ce8192fbc75a6a
Kristian Niemi <kristian.niemi@gmail.com>
2023-02-03 09:17:11 +0100

    indentation

--- users/krissen/rules.mk
+++ users/krissen/rules.mk
@@ -4,13 +4,13 @@ SRC += krissen.c
 SRC += process_records.c
 
 ifeq ($(strip $(OLED_ENABLE)), yes)
-    SRC += oled_stuff.c
+	SRC += oled_stuff.c
 endif
 
 ifeq ($(strip $(ENCODER_ENABLE)), yes)
-    SRC += encoder_stuff.c
+	SRC += encoder_stuff.c
 endif
 
 ifeq ($(strip $(PIMORONI_TRACKBALL_ENABLE)), yes)
-    SRC += pimoroni_trackball.c
+	SRC += pimoroni_trackball.c
 endif
