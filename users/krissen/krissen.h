df9472d496e182712685d9f236c460ee124e4a56
Kristian Niemi <kristian.niemi@gmail.com>
2023-02-05 13:33:04 +0100

    Tagit bort sånt jag inte använder

--- users/krissen/krissen.h
+++ users/krissen/krissen.h
@@ -36,23 +36,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum userspace_layers {
 	_COLEMAK = 0,
 	_QWERTY,
-	_GAMING,
 	_SHORTCUT,
 	_SYMNUM,
 	_NAVIGATION,
-#ifdef DICE_ROLLER_ENABLE
-	_RPG,
-#endif
-
 };
 
 #define CADEL LALT(LCTL(KC_DELETE))
 #define TRML LALT(LCTL(KC_T))
 #define TSKMN LCTL(LSFT(KC_ESCAPE))
-#define CLSTAB LCTL(LSFT(KC_T))
-#define SNAP LGUI(LSFT(KC_S))
-#define SNAPL LGUI(LSFT(KC_LEFT))
-#define SNAPR LGUI(LSFT(KC_RGHT))
 
 #define _____________________QWERTY_L1______________________ KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T
 #define _____________________QWERTY_L2______________________ KC_DEL, LALT_T(KC_A), KC_S, KC_D, KC_F, KC_G
@@ -67,8 +58,8 @@ enum userspace_layers {
 #define _____________________QWER_TH_R2_____________________ OSL(_NAVIGATION), C(KC_BSPC)
 
 #define _____________________COLEMAK_L1_____________________ KC_ESC,        KC_Q,  KC_W, KC_F, KC_P, KC_B
-#define _____________________COLEMAK_L2_____________________ LGUI_T(KC_DEL),LCTL_T(KC_A), KC_R, KC_S, KC_T, KC_G
-#define _____________________COLEMAK_L3_____________________ KC_LALT,       LALT_T(KC_Z), KC_X, KC_C, KC_D, KC_V
+#define _____________________COLEMAK_L2_____________________ LGUI_T(LALT(KC_4)),LCTL_T(KC_A), KC_R, KC_S, KC_T, KC_G
+#define _____________________COLEMAK_L3_____________________ LT(_NAVIGATION, LSFT(KC_RBRC)), LALT_T(KC_Z), KC_X, KC_C, KC_D, KC_V
 #define _____________________COLE_TH_L1_____________________ TO(_NAVIGATION), KC_LEFT, MT(MOD_LSFT, KC_TAB), LT(_SYMNUM, KC_ENT), KC_BSPC
 #define _____________________COLE_TH_L2_____________________ OSM(MOD_LSFT), KC_DOWN
 
@@ -78,8 +69,8 @@ enum userspace_layers {
 #define _____________________COLE_TH_R1_____________________ KC_BSPC, MT(MOD_RSFT, KC_SPC), KC_DEL, KC_RIGHT, TO(_QWERTY)
 #define _____________________COLE_TH_R2_____________________ OSL(_NAVIGATION), KC_UP
 
-#define _____________________SHORTCT_L1_____________________ RESET,_______,C(KC_W),C(KC_F),SNAPL,SNAPR
-#define _____________________SHORTCT_L2_____________________ TRML, _______, _______, SNAP, CLSTAB, _______
+#define _____________________SHORTCT_L1_____________________ RESET,_______,C(KC_W),C(KC_F),_______,_______
+#define _____________________SHORTCT_L2_____________________ TRML, _______, _______, _______, _______, _______
 #define _____________________SHORTCT_L3_____________________ TSKMN, _______, _______, _______, LALT(LCTL(KC_V)),_______
 #define _____________________SHRT_TH_L1_____________________ G(KC_TAB), G(C(KC_LEFT)), G(C(KC_RIGHT)), RGB_TOG, RGB_M_P
 #define _____________________SHRT_TH_L2_____________________ _______,_______
@@ -97,17 +88,5 @@ enum userspace_layers {
 #define _____________________SYMNUM_RT1_____________________ TD(TD_ASDI),KC_7,KC_8,KC_9,_______,_______
 #define _____________________SYMNUM_RT2_____________________ TD(TD_PLMN),KC_4,KC_5,KC_6,KC_0,_______
 #define _____________________SYMNUM_RT3_____________________ _______,    KC_1,KC_2,KC_3,KC_DOT,_______
-
-#define _____________________GAMING_LF1_____________________ KC_ESC,    KC_I,    KC_Q,    KC_W,    KC_E,    KC_R
-#define _____________________GAMING_LF2_____________________ KC_LCTRL, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F
-#define _____________________GAMING_LF3_____________________ KC_GRV, CTL_T(KC_C),KC_Z,    KC_X,    KC_C,    KC_V
-#define _____________________GAME_TH_L1_____________________ TO(_COLEMAK),  OSL(_NAVIGATION), KC_SPC,  KC_LALT,  KC_ENT
-#define _____________________GAME_TH_L2_____________________ KC_TAB, KC_0
-
-#define _____________________GAMING_RT1_____________________ KC_T,    TD(TD_7),    TD(TD_8),    TD(TD_9),    KC_O,    KC_P
-#define _____________________GAMING_RT2_____________________ KC_G,    TD(TD_4),    TD(TD_5),    TD(TD_6),    TD(TD_0), KC_SCLN
-#define _____________________GAMING_RT3_____________________ KC_B,    TD(TD_1),    TD(TD_2),    TD(TD_3),  KC_DOT, KC_SLASH
-#define _____________________GAME_TH_R1_____________________ KC_F5,  KC_F8,  KC_F18,  KC_F17,  TO(_COLEMAK)
-#define _____________________GAME_TH_R2_____________________ KC_F10,  KC_F12
 
 #define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
--- users/krissen/oled_stuff.c
+++ users/krissen/oled_stuff.c
@@ -19,16 +19,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef OLED_ENABLE
 
 #ifdef WPM_ENABLE
-	//#include "wpm_oled.c"
-	#include "wpm_cat.c"
-	char wpm_str[10];
+//#include "wpm_oled.c"
+#include "wpm_cat.c"
+char wpm_str[10];
 #endif
 
 extern uint8_t is_master;
 
 void render_mod_status(uint8_t modifiers) {
-    oled_write_P(PSTR(OLED_RENDER_MODS_NAME), false);
-	
+	oled_write_P(PSTR(OLED_RENDER_MODS_NAME), false);
+
 	if (modifiers & MOD_MASK_SHIFT){oled_write_P(shift_key, false);}
 	if (modifiers & MOD_MASK_CTRL){oled_write_P(ctrl_key, false);}
 	if (modifiers & MOD_MASK_ALT){oled_write_P(alt_key, false);}
@@ -37,46 +37,35 @@ void render_mod_status(uint8_t modifiers) {
 }
 
 static void render_status(void) {
-    // QMK Logo and version information
-    oled_write_P(logo, false);
+	// QMK Logo and version information
+	oled_write_P(logo, false);
 	oled_advance_page(true);
 	oled_advance_page(true);
 
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR(OLED_RENDER_LAYOUT_NAME), false);
-    switch (get_highest_layer(layer_state)) {
-        case _COLEMAK:
-            oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_COLEMAK), false);
-            break;
-        case _SYMNUM:
-            oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_SYMNUM), false);
-            break;
-        case _SHORTCUT:
-            oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_SHORTCUT), false);
-            break;
-		case _GAMING:
-            oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_GAMING), false);
-            break;
-		//case _WARHAMMER:
-          //  oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_WARHAMMER), false);
-            //break;
+	// Host Keyboard Layer Status
+	oled_write_P(PSTR(OLED_RENDER_LAYOUT_NAME), false);
+	switch (get_highest_layer(layer_state)) {
+		case _COLEMAK:
+			oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_COLEMAK), false);
+			break;
+		case _SYMNUM:
+			oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_SYMNUM), false);
+			break;
+		case _SHORTCUT:
+			oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_SHORTCUT), false);
+			break;
 		case _NAVIGATION:
-            oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_NAVIGATION), false);
-            break;
-		#ifdef DICE_ROLLER_ENABLE
-		case _RPG:
-            oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_RPG), false);
-            break;
-		#endif
+			oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_NAVIGATION), false);
+			break;
 		case _QWERTY:
-            oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_QWERTY), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
+			oled_write_ln_P(PSTR(OLED_RENDER_LAYOUT_QWERTY), false);
+			break;
+		default:
+			// Or use the write_ln shortcut over adding '\n' to the end of your string
+			oled_write_ln_P(PSTR("Undefined"), false);
+	}
 
-    // Host Keyboard LED Status
+	// Host Keyboard LED Status
 	led_t led_state = host_keyboard_led_state();
 	oled_write_P(PSTR(OLED_RENDER_LOCK_NAME), false);
 	oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
@@ -88,9 +77,9 @@ static void render_status(void) {
 
 
 bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-		render_status();
-        return false;
+	if (is_keyboard_master()) {
+	render_status();
+		return false;
 	} else {
 
 		#ifdef WPM_ENABLE
