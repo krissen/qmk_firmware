2b17ad16f13c21cc8838e7eae91ba3605ad5b132
Kristian Niemi <kristian.niemi@gmail.com>
2023-02-12 21:19:28 +0100

    Ny tapdance: tick

--- keyboards/handwired/dactyl_coolio/keymaps/krissen/keymap.c
+++ keyboards/handwired/dactyl_coolio/keymaps/krissen/keymap.c
@@ -18,21 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Enumerate the tap dances
 enum taps {
 	TD_GUIDOL,
-	TD_EXLM, //! and ?
-	TD_PCAS, //% and *
-	TD_PLMN, //+ and -
-	TD_ASDI, //* and /
-	TD_TABC, //Tab and Ctrl TAB
-	TD_0,
-	TD_1,
-	TD_2,
-	TD_3,
-	TD_4,
-	TD_5,
-	TD_6,
-	TD_7,
-	TD_8,
-	TD_9
+	TD_TICK
 };
 
 // define a type containing as many tapdance states as you need
@@ -51,6 +37,8 @@ int cur_dance (qk_tap_dance_state_t *state);
 // `finished` and `reset` functions for each tapdance keycode
 void guidol_finished (qk_tap_dance_state_t *state, void *user_data);
 void guidol_reset (qk_tap_dance_state_t *state, void *user_data);
+void tick_finished (qk_tap_dance_state_t *state, void *user_data);
+void tick_reset (qk_tap_dance_state_t *state, void *user_data);
 
 // determine the tapdance state to return
 int cur_dance (qk_tap_dance_state_t *state) {
@@ -78,6 +66,21 @@ void guidol_finished (qk_tap_dance_state_t *state, void *user_data) {
 			register_code16(KC_LPRN);
 	}
 }
+void tick_finished (qk_tap_dance_state_t *state, void *user_data) {
+	td_state = cur_dance(state);
+	switch (td_state) {
+		case SINGLE_TAP:
+			register_mods(MOD_BIT(KC_RSFT));
+			register_code16(KC_EQUAL);
+			break;
+		case SINGLE_HOLD:
+			register_mods(MOD_BIT(KC_RGUI)); // for a layer-tap key, use `layer_on(_MY_LAYER)` here
+			break;
+		case DOUBLE_SINGLE_TAP: // allow nesting of 2 parens `((` within tapping term
+			tap_code16(KC_EQUAL);
+			register_code16(KC_EQUAL);
+	}
+}
 
 void guidol_reset (qk_tap_dance_state_t *state, void *user_data) {
 	switch (td_state) {
@@ -93,27 +96,24 @@ void guidol_reset (qk_tap_dance_state_t *state, void *user_data) {
 	}
 }
 
+void tick_reset (qk_tap_dance_state_t *state, void *user_data) {
+	switch (td_state) {
+		case SINGLE_TAP:
+			unregister_code16(KC_EQUAL);
+			unregister_mods(MOD_BIT(KC_RSFT));
+			break;
+		case SINGLE_HOLD:
+			unregister_mods(MOD_BIT(KC_LGUI)); // for a layer-tap key, use `layer_off(_MY_LAYER)` here
+			break;
+		case DOUBLE_SINGLE_TAP:
+			unregister_code16(KC_EQUAL);
+	}
+}
+
 // TAP DANCING
 qk_tap_dance_action_t tap_dance_actions[] = {
 	[TD_GUIDOL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, guidol_finished, guidol_reset),
-	/* IDB3:s below */
-	//[TD_PRNS] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
-	//[TD_BRCS] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
-	[TD_EXLM] = ACTION_TAP_DANCE_DOUBLE(KC_EXLM, KC_QUES),
-	[TD_PCAS] = ACTION_TAP_DANCE_DOUBLE(KC_PERC, KC_ASTR),
-	[TD_PLMN] = ACTION_TAP_DANCE_DOUBLE(KC_PPLS, KC_PMNS),
-	[TD_ASDI] = ACTION_TAP_DANCE_DOUBLE(KC_PAST, KC_PSLS),
-	[TD_TABC] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, S(KC_TAB)),
-	[TD_0] = ACTION_TAP_DANCE_DOUBLE(KC_0, C(KC_0)),
-	[TD_1] = ACTION_TAP_DANCE_DOUBLE(KC_1, C(KC_1)),
-	[TD_2] = ACTION_TAP_DANCE_DOUBLE(KC_2, C(KC_2)),
-	[TD_3] = ACTION_TAP_DANCE_DOUBLE(KC_3, C(KC_3)),
-	[TD_4] = ACTION_TAP_DANCE_DOUBLE(KC_4, C(KC_4)),
-	[TD_5] = ACTION_TAP_DANCE_DOUBLE(KC_5, C(KC_5)),
-	[TD_6] = ACTION_TAP_DANCE_DOUBLE(KC_6, C(KC_6)),
-	[TD_7] = ACTION_TAP_DANCE_DOUBLE(KC_7, C(KC_7)),
-	[TD_8] = ACTION_TAP_DANCE_DOUBLE(KC_8, C(KC_8)),
-	[TD_9] = ACTION_TAP_DANCE_DOUBLE(KC_9, C(KC_9))
+	[TD_TICK] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tick_finished, tick_reset),
 };
 
 // KEYMAPS
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_SYMBOLS] = LAYOUT_wrapper(
 		KC_F1,		KC_F2,      KC_F3, 	KC_F4, 	       KC_F5,	   KC_F6,		KC_F7,      KC_F8,          KC_F9,         KC_F10,  	     KC_F11,	       KC_F12,
 		KC_NUBS,	LALT(KC_8), LALT(KC_9), LALT(KC_RBRC), LALT(KC_7), LALT(LSFT(KC_7)),	RSFT(KC_7), RSFT(KC_MINUS), RSFT(KC_BSLS), RALT(RSFT(KC_8)), RALT(RSFT(KC_9)), RSFT(KC_2),
-		KC_GRAVE, LSFT(KC_1), LALT(KC_2), LSFT(KC_3),    LSFT(KC_4), LSFT(KC_5),		RSFT(KC_6), RALT(KC_3),     RSFT(KC_8),    RSFT(KC_9),       RALT(KC_SLSH),    KC_EQUAL,
+		KC_GRAVE, LSFT(KC_1), LALT(KC_2), LSFT(KC_3),    LSFT(KC_4), LSFT(KC_5),		RSFT(KC_6), RALT(KC_3),     RSFT(KC_8),    RSFT(KC_9),       RALT(KC_EQUAL),    TD(TD_TICK),
 		_______, KC_UP, TO(_COLEMAK), _______, _______,       	    			_______, _______, TO(_COLEMAK), KC_DOWN, _______,
 		_______, _______,                                  	    				_______, _______
 	),
